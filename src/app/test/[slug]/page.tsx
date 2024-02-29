5bcde7445cc6637367b81edf56d81919d7aed73d
socar-rio <alstn4814@gmail.com>
2024-02-29 21:39:26 +0900

    test

---
+++ src/app/test/[slug]/page.tsx
@@ -0,0 +1,46 @@
+// import { notFound } from "next/navigation"
+
+import { revalidatePath } from "next/cache"
+
+// export const generateMetadata = ({ params }: Props) => {
+//   return {
+//     title: `${params.slug} 제품 이름`,
+//     description: "My Notes description",
+//     icons: {
+//       icon: "/favicon.ico",
+//     },
+//   }
+// }
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+type Data = {
+  data: string[]
+}
+
+export default async function TestPage({ params }: Props) {
+  console.log(params.slug)
+  const res = await fetch(`https://meowfacts.herokuapp.com?${params.slug}`, {
+    next: { tags: ["a"] },
+  })
+  const data: Data = await res.json()
+  const text = data.data[0]
+
+  return (
+    <div>
+      <h1>{params.slug} 제품 설명 페이지</h1>
+      <div>{text}</div>
+    </div>
+  )
+}
+
+// export function generateStaticParams() {
+//   const products = ["pants", "skirt", "test2"]
+//   return products.map((product) => ({
+//     slug: product,
+//   }))
+// }
